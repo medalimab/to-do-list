ff9fff9dca14560a1a7624cc328ab7a30a33117a
dali <mabroukdali1925@gmail.com>
2024-08-22 14:12:55 +0200

    update Interface

--- src/Interface/TaskListProps.ts
+++ src/Interface/TaskListProps.ts
@@ -1,9 +1,12 @@
-
-import { Task } from "../Interface/Task"; 
+import { Task } from "../Interface/Task";
 
 export interface TaskListProps {
   tasks: Task[];
   setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
   completedTask: Task[];
   setCompletedTask: React.Dispatch<React.SetStateAction<Task[]>>;
+  blockedTasks: Task[]; 
+  setBlockedTasks: React.Dispatch<React.SetStateAction<Task[]>>; 
+  inProgressTasks: Task[]; 
+  setInProgressTasks: React.Dispatch<React.SetStateAction<Task[]>>; 
 }
